30a56e555f4252f9da65003b15d8091175c2e52a
olymuzyka-intensive <137098489+olymuzyka-intensive@users.noreply.github.com>
2024-08-15 01:49:35 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./modules/Header";
+import { Main } from "./modules/Main";
+import { Footer } from "./modules/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
