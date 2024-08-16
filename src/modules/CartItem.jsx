b21ba11adbea0b6b1909d6260b9fbc94cb8a12b2
olymuzyka-intensive <137098489+olymuzyka-intensive@users.noreply.github.com>
2024-08-16 22:58:25 +0300

    add cart + css

--- src/modules/CartItem.jsx
+++ src/modules/CartItem.jsx
@@ -1,5 +1,5 @@
-export const CartItem = (data) => {
-  <li className="cart-item">
+export const CartItem = ({data}) => {
+  return (<li className="cart-item">
     <img className="cart-item__image" src={data.image} alt={data.title} />
     <div className="cart-item__info">
       <h3 className="cart-item__title">{data.title}</h3>
@@ -7,8 +7,9 @@ export const CartItem = (data) => {
         <button className='cart-item__quantity-button cart-item__quantity-button_minus'></button>
         <input type="number" className="cart-item__quantity-input" value={1} />
         <button className="cart-item__quantity-button cart-item__quantity-button_plus"></button>
-        <p className="product__price">{data.price} ₽</p>
       </div>
+      <p className="cart-item__price">{data.price}&nbsp;₽</p>
     </div>
-  </li>;
+  </li>
+  )
 };
