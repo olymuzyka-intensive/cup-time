844113341c39e833073b6cab61097db15e13d49b
olymuzyka-intensive <137098489+olymuzyka-intensive@users.noreply.github.com>
2024-08-17 01:43:25 +0300

    add api + modal

---
+++ src/const.js
@@ -0,0 +1,4 @@
+export const API_URL = 'https://cup-time-api-dfbh.onrender.com';
+
+// /api/products/{category} 
+
--- src/modules/Products.jsx
+++ src/modules/Products.jsx
@@ -1,7 +1,19 @@
-import { products } from "../products";
+import { useEffect } from "react";
+// import { products } from "../products";
 import { Product } from "./Product";
+import { useProducts } from "../context/ProductContext";
+import { useSearchParams } from "react-router-dom";
+import { API_URL } from "../const"
 
 export const Products = () => {
+  const [seachParams] = useSearchParams()
+  const { products, setCategory} = useProducts();
+  const category = seachParams.get('category');
+
+  useEffect(() => {
+    setCategory(category);
+  }, [category, setCategory])
+
   return (
     <section className="products">
       <div className="container">
