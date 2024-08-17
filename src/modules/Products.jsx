ea941819621e2a88d73bb8e9a944bab32557bef5
olymuzyka-intensive <137098489+olymuzyka-intensive@users.noreply.github.com>
2024-08-17 10:49:48 +0300

    add title category

--- src/modules/Products.jsx
+++ src/modules/Products.jsx
@@ -17,7 +17,7 @@ export const Products = () => {
   return (
     <section className="products">
       <div className="container">
-        <h2 className="products__title">Чай</h2>
+        <h2 className="products__title">{category}</h2>
 
         <ul className="products__list">
           {products.map((item) => (<Product key={item.id} data={item}/>))}
