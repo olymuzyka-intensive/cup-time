6acd0292ef492f33a66ae02df087da4d74132b99
olymuzyka-intensive <137098489+olymuzyka-intensive@users.noreply.github.com>
2024-08-17 03:01:53 +0300

    add css modal

--- src/modules/Product.jsx
+++ src/modules/Product.jsx
@@ -23,7 +23,7 @@ export const Product = ({data}) => {
             />
             <div className="product__content">
               <h3 className="product__title">{data.title}</h3>
-              <p className="product__price">{data.price} ₽</p>
+              <p className="product__price">{data.price}&nbsp;₽</p>
             </div>
           </article>
         </a>
--- src/modules/ProductModal.jsx
+++ src/modules/ProductModal.jsx
@@ -9,6 +9,7 @@ const customStyles = {
     bottom: "auto",
     marginRight: "-50%",
     transform: "translate(-50%, -50%)",
+    padding: '0'
   },
 };
 
@@ -26,17 +27,32 @@ export const ProductModal = ({ isOpen, onRequestClose, data }) => {
       style={customStyles}
       contentLabel="Product Modal"
     >
-    <h2>{data.title}</h2>
-    <img src={`${API_URL}${data.img}`} alt={data.title} />
-    <p>{data.price}</p>
-    <ul>
-        {Object.entries(data.additional).map(([key, value]) => (
-            <li key={key}>
-                <strong>{key}</strong> {value}
-            </li>
-        ))}
-    </ul>
-    <button onClick={onRequestClose}>x</button>
+    <div className="modal">
+        <img className="modal__img" src={`${API_URL}${data.img}`} alt={data.title} />
+        <div className="modal__info">
+            <div className="modal__info-top">
+                <h3 className="modal__title">{data.title}</h3>
+                <p className="modal__price">{data.price}&nbsp;₽</p>
+            </div>
+            <ul className="modal__items">
+                {Object.entries(data.additional).map(([key, value]) => (
+                    <li key={key} className="modal-item">
+                        {/* <strong>{key}</strong> {value} */}
+                        <div className="modal-item__key">{key}</div> <div className="modal-item__value">{value}</div>
+                    </li>
+                ))}
+            </ul>
+            <div className="modal-item__quantity">
+                <button className='modal-item__quantity-button cart-item__quantity-button_minus'></button>
+                <input type="number" className="modal-item__quantity-input" value={1} />
+                <button className="modal-item__quantity-button cart-item__quantity-button_plus"></button>
+                <button className="modal__order-button">Добавить</button>
+            </div>
+
+            <button className="modal__close" onClick={onRequestClose}>+</button>
+        </div>
+    </div>
+    
 
     </Modal>
   );
